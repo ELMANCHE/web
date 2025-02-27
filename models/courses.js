8aa7163168b39378649ca5fe940816da107ac048
ELMANCHE <110752573+ELMANCHE@users.noreply.github.com>
2025-02-27 10:03:05 -0500

    hola

---
+++ models/courses.js
@@ -0,0 +1,16 @@
+'use strict'
+
+var mongoose = require('mongoose');
+
+
+var Schema = mongoose.Schema;
+
+var courseSchema = Schema([
+    name : String,
+    duration : Number,
+    price: Number
+
+
+
+
+]);
